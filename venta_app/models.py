9541a53039a5a9b84c0a253e4618ff2c828db740
Adrian-sosaa <a.22308051281317@cbtis128.edu.mx>
2024-12-04 11:34:55 -0600

    Add files via upload

---
+++ venta_app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Venta(models.Model):
+    id_venta = models.IntegerField(null=False, primary_key=True)
+    nombre = models.CharField(max_length=100, null=False)
+    hora_compra = models.TimeField(null=False)
+    contenido = models.CharField(max_length=200, null=False)
+    cantidad = models.IntegerField(null=False)
+    total = models.DecimalField(max_digits=10, decimal_places=2, null=False)
+    nom_cliente = models.CharField(max_length=50, null=False)
---
+++ venta_app/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render, redirect
+from .models import Venta
+
+def inicio_vistaVentas(request):
+    lasVentas = Venta.objects.all()
+    return render(request, "gestionarVentas.html", {"misVentas": lasVentas})
+
+def registrarVenta(request):
+    id_venta = request.POST["numIdVenta"]
+    nombre = request.POST["txtNombre"]
+    hora_compra = request.POST["txtHoraCompra"]
+    contenido = request.POST["txtContenido"]
+    cantidad = request.POST["numCantidad"]
+    total = request.POST["numTotal"]
+    nom_cliente = request.POST["txtNomCliente"]
+
+    Venta.objects.create(
+        id_venta=id_venta,
+        nombre=nombre,
+        hora_compra=hora_compra,
+        contenido=contenido,
+        cantidad=cantidad,
+        total=total,
+        nom_cliente=nom_cliente,
+    )
+
+    return redirect("ventas")
+
+def seleccionarVenta(request, id_venta):
+    venta = Venta.objects.get(id_venta=id_venta)
+    return render(request, "editarVentas.html", {"miVenta": venta})
+
+def editarVenta(request):
+    id_venta = request.POST["numIdVenta"]
+    nombre = request.POST["txtNombre"]
+    hora_compra = request.POST["txtHoraCompra"]
+    contenido = request.POST["txtContenido"]
+    cantidad = request.POST["numCantidad"]
+    total = request.POST["numTotal"]
+    nom_cliente = request.POST["txtNomCliente"]
+
+    venta = Venta.objects.get(id_venta=id_venta)
+    venta.nombre = nombre
+    venta.hora_compra = hora_compra
+    venta.contenido = contenido
+    venta.cantidad = cantidad
+    venta.total = total
+    venta.nom_cliente = nom_cliente
+    venta.save()
+
+    return redirect("ventas")
+
+def borrarVenta(request, id_venta):
+    venta = Venta.objects.get(id_venta=id_venta)
+    venta.delete()
+    return redirect("ventas")
